5245c6cd13b0477b9732c43150aa42b268f6c134
Valter Gabriel <vgabrielbri@hotmail.com>
2022-04-25 21:51:42 -0300

    Primeiro commit

---
+++ src/main/java/com/FindMySquad/FindMySquad/FindMySquadApplication.java
@@ -0,0 +1,13 @@
+package com.FindMySquad.FindMySquad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindMySquadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindMySquadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/FindMySquad/FindMySquad/FindMySquadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FindMySquad.FindMySquad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindMySquadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
