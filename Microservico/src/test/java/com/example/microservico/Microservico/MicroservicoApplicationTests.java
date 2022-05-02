b6f99026bbe8696c1c2cbb1abcfc8c839a7993ea
Valter Gabriel <vgabrielbri@hotmail.com>
2022-05-02 11:31:32 -0300

    Configurando o discovery service

---
+++ Microservico/src/test/java/com/example/microservico/Microservico/MicroservicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microservico.Microservico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
